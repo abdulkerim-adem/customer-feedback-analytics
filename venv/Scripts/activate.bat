1c4d95323e631d6dfa01f2fb1b5c1874e8cb7fe4
Abdulkerim Adem Amano <abdulkerima@hijrabank.local>
2025-06-12 18:02:50 +0300

    feat: Insert into db, export database_dump

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\h_user\IdeaProjects\customer-feedback-analytics\venv
+set VIRTUAL_ENV=C:\Users\abdulkerima\IdeaProjects\customer-feedback-analytics\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
